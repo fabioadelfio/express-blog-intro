6d04ae5f015f533605bb569c0a49ff91ea9d1f23
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-12 14:59:49 +0200

    add posts

--- PersonalBlog/app.js
+++ PersonalBlog/app.js
@@ -5,8 +5,45 @@ const port = 3000;
 app.use(express.static(`img`));
 
 app.get("/", (req, res) => {
-    res.send(`Server Personal Blog`);
+    res.send(`Server del mio blog`);
 });
+
+const posts = [
+    {
+        titolo: `Ciambellone`,
+        contenuto: `Ricetta ciambellone`,
+        immagine: `/img/ciambellone.jpeg`,
+        tags: [`ciambellone`, `dolce`],
+    },
+
+    {
+        titolo: `Cracker Barbabietola`,
+        contenuto: `Ricetta cracker barbabietola`,
+        immagine: `/img/cracker_barbabietola.jpeg`,
+        tags: [`cracker`, `barbabietola`],
+    },
+
+    {
+        titolo: `Pane Fritto Dolce`,
+        contenuto: `Ricetta pane fritto dolce`,
+        immagine: `/img/pane_fritto_dolce.jpeg`,
+        tags: [`pane`, `fritto`, `dolce`],
+    },
+
+    {
+        titolo: `Pasta Barbabietola`,
+        contenuto: `Ricetta pasta barbabietola`,
+        immagine: `/img/pasta_barbabietola.jpeg`,
+        tags: [`pasta`, `barbabietola`],
+    },
+
+    {
+        titolo: `Torta Paesana`,
+        contenuto: `Ricetta torta paesana`,
+        immagine: `/img/torta_paesana.jpeg`,
+        tags: [`torta`, `dolce`, `paese`],
+    }
+]
 
 app.listen(port, () => {
     console.log(`Il server è in ascolto sulla porta ` + port);
